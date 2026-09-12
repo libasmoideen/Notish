2fa84de7237f9649ff8c5ff53fde54756445d360
libasmoideen <libasmoideen425@gmail.com>
2026-09-12 18:02:06 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "notish-v11";
+const CACHE = "notish-v12";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
